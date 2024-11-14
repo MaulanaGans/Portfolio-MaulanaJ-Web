e159d6fbbd0a0362f4bbe8164c42ec0ccea41b54
MaulanaGans <67085249+MaulanaGans@users.noreply.github.com>
2024-11-14 22:00:17 +0700

    Add files via upload

--- experience/script.js
+++ experience/script.js
@@ -65,8 +65,8 @@ document.onkeydown = function(e) {
 document.addEventListener('visibilitychange',
 function(){
     if(document.visibilityState === "visible"){
-        document.title = "Experience | Portfolio Jigar Sable";
-        $("#favicon").attr("href","/assets/images/favicon.png");
+        document.title = "Experience | Portfolio Maulana Jumantara";
+        $("#favicon").attr("href","/assets/images/Pp.png");
     }
     else {
         document.title = "Come Back To Portfolio";
