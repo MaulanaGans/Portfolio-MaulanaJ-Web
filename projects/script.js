fe5ff48a35c469a25659acd7e3832278db867fe6
MaulanaGans <67085249+MaulanaGans@users.noreply.github.com>
2024-11-15 14:14:18 +0700

    Add files via upload

--- projects/script.js
+++ projects/script.js
@@ -20,12 +20,12 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Projects | Portfolio Jigar Sable";
-            $("#favicon").attr("href", "/assets/images/favicon.png");
+            document.title = "Projects | Portfolio Maulana Jumantara";
+            $("#Pp").attr("href", "/assets/images/Pp.png");
         }
         else {
             document.title = "Come Back To Portfolio";
-            $("#favicon").attr("href", "/assets/images/favhand.png");
+            $("#Pp").attr("href", "/assets/images/favhand.png");
         }
     });
 
